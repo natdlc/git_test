f16a70f02ed687424c1928b834575d8ed146defd
Nat Corpuz <nate.corpuz1@gmail.com>
2022-02-01 11:19:17 +0800

    logical operators exercise

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
-let text = "hELLo world";
-console.log(text);
-let newText = text.toUpperCase();
-console.log(newText);
-newText = text.toLowerCase();
-console.log(newText);
+/* 
+
+2
+alert(1)
+null
+alert(2)
+3
+
+
+*/
+
+// let age = 91;
+
+// if (age >= 14 && age <= 90) {
+//     console.log("eligible");
+// }
+
+// else {
+//     console.log("not eligible");
+// }
+
+
+/* ~~~ */
+
+
+// if (!(age >= 14 && age <= 90)) {
+//     console.log("not eligible");
+// }
+
+
+/* ~~~ */
+
+
+/* 
+
+true first
+false no alert
+true third
+
+*/
+
+
+/* ~~~ */
+
+
+let user = prompt("Who's there?");
+
+if (user === "Admin") {
+
+    let password = prompt("Password?");
+
+    if (password === "TheMaster") {
+        alert("Welcome!");
+    }
+
+    else if (password === null || password ==="") {
+        alert("Canceled");
+    }
+
+    else {
+        alert("Wrong password");
+    }
+
+}
+else if (user === null || user === "") {
+    alert("Canceled");
+}
+
+else {
+    alert("I don't know you");
+}
