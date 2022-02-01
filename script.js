3f167b685b03da269e02ef249f3fe7b3902ce492
Nat Corpuz <nate.corpuz1@gmail.com>
2022-02-01 17:01:55 +0800

    switch review

--- script.js
+++ script.js
@@ -1,72 +1,16 @@
-/* 
+let x = 'purple';
 
-o 2
-x alert(1)
-o null
-x alert(2)
-o 3
-
-
-*/
-
-// let age = 91;
-
-// o if (age >= 14  && age <= 90) {
-//     console.log("eligible");
-// }
-
-// else {
-//     console.log("not eligible");
-// }
-
-
-/* ~~~ */
-
-
-// o if (!(age >= 14 && age <= 90)) {
-//     console.log("not eligible");
-// } incomplete
-
-
-
-/* ~~~ */
-
-
-/* 
-
-o true first
-o false no alert
-o true third
-
-*/
-
-
-/* ~~~ */
-
-
-let user = prompt("Who's there?");
-
-if (user === "Admin") {
-
-    let password = prompt("Password?");
-
-    if (password === "TheMaster") {
-        alert("Welcome!");
-    }
-
-    else if (password === null || password ==="") {
-        alert("Canceled");
-    }
-
-    else {
-        alert("Wrong password");
-    }
-
-}
-else if (user === null || user === "") {
-    alert("Canceled");
-}
-
-else {
-    alert("I don't know you");
+switch (x) {
+    case 'black':
+    case 'red':
+    case 'white':
+        console.log('paired');
+        break;
+    case 'orange':
+    case 'purple':
+    case 'green':
+        console.log('reverse-paired');
+        break;
+    default:
+        console.log('error');
 }
