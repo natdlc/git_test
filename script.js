b703f3761372ecfb5d40d9e1f59debd02d71495e
Nat Corpuz <nate.corpuz1@gmail.com>
2022-02-02 12:48:17 +0800

    simple login interface

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-let radius = +prompt("Enter radius");
+let loginAns = prompt('Greetings! Would you like to log in? Y/N');
 
-radius === 0 ? alert('please enter valid number') :
-    !(radius > 0 || radius < 0) ? alert('please enter a number') :
-    alert(circumference(radius));
+loginGreet(loginAns);
 
-function circumference(rad) {
-    return rad * Math.PI * 2;
+function loginGreet(ans='') {
+    if (ans === 'y' || ans === 'Y') {
+        let username = prompt('Enter username');
+        enterUsername(username);
+    }
+
+    else {
+        alert('login cancelled');
+    }
+}
+
+function enterUsername(uname = '') {
+    if (uname === 'oriusprime') {
+        let password = prompt('Enter password');
+        enterPassword(password);
+    }
+
+    else {
+        alert('sorry, no such username exists');
+    }
+}
+
+function enterPassword(upass = '') {
+    if (upass === '12three') {
+        alert('You\'re logged in!');
+    }
+
+    else {
+        alert('invalid password, please try again');
+        loginGreet(loginAns);
+    }
 }
