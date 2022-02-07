ace3bcfa67e92eb161361749d5e290705be2a210
Nat Corpuz <nate.corpuz1@gmail.com>
2022-02-08 06:27:47 +0800

    dom manipulation

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-const container = document.querySelector('.container');
-const child2 = document.querySelector('.box-2');
-let newDiv = document.createElement('div');
-newDiv.classList.add('new');
-newDiv.textContent = 'hello';
-container.insertBefore(newDiv, child2);
-container.removeChild(child2);
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+//red paragraph
+const redPara = document.createElement('p');
+redPara.style.color = 'red';
+redPara.textContent = 'Hey I\'m red!';
+container.appendChild(redPara);
+
+//blue h3
+const blueH3 = document.createElement('h3');
+blueH3.style.color = 'blue';
+blueH3.textContent = 'I\'m a blue h3!';
+container.appendChild(blueH3);
+
+//div black border
+const divBlack = document.createElement('div');
+divBlack.style.border = '1px solid black';
+divBlack.style.backgroundColor = 'lightpink';
+divBlack.classList.add('black-div');
+container.appendChild(divBlack);
+
+//h1 inside div black
+const h1Child = document.createElement('h1');
+h1Child.textContent = 'I\'m in a div';
+divBlack.appendChild(h1Child);
+
+//p inside div black
+const pChild = document.createElement('p');
+pChild.textContent = 'ME TOO!';
+divBlack.appendChild(pChild);
