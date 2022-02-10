f6a6075b5ed27d8dd70c7a868a2f1d96d3abb8cd
Nat Corpuz <nate.corpuz1@gmail.com>
2022-02-10 13:50:31 +0800

    edit

--- script.js
+++ script.js
@@ -1,55 +1,7 @@
-// const container = document.querySelector('#container');
+let man = prompt('who\'s there?');
 
-// const content = document.createElement('div');
-// content.classList.add('content');
-// content.textContent = 'This is the glorious text-content!';
-
-// container.appendChild(content);
-
-// //red paragraph
-// const redPara = document.createElement('p');
-// redPara.style.color = 'red';
-// redPara.textContent = 'Hey I\'m red!';
-// container.appendChild(redPara);
-
-// //blue h3
-// const blueH3 = document.createElement('h3');
-// blueH3.style.color = 'blue';
-// blueH3.textContent = 'I\'m a blue h3!';
-// container.appendChild(blueH3);
-
-// //div black border
-// const divBlack = document.createElement('div');
-// divBlack.style.border = '1px solid black';
-// divBlack.style.backgroundColor = 'lightpink';
-// divBlack.classList.add('black-div');
-// container.appendChild(divBlack);
-
-// //h1 inside div black
-// const h1Child = document.createElement('h1');
-// h1Child.textContent = 'I\'m in a div';
-// divBlack.appendChild(h1Child);
-
-// //p inside div black
-// const pChild = document.createElement('p');
-// pChild.textContent = 'ME TOO!';
-// divBlack.appendChild(pChild);
-
-// //button
-// const btn = document.createElement('button');
-// btn.textContent = 'click me';
-// btn.onclick = clickers;
-// container.appendChild(btn);
-
-// function clickers(e) {
-//     console.log(e);
-// }
-
-function testFunc1 () {
-    return 'first function';
+let human = {
+    [man]: 'nate',
 }
 
-function testFunc2 (callback) {
-    return callback;
-}
-
+alert(human.nate);
