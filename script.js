4b04d90848ec377ef6705667eb53af222547b6ab
nat corpuz <nate.corpuz1@gmail.com>
2022-03-13 21:29:18 +0800

    factory functions

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-let func1 = function (e) {
-    console.log(this);
-    console.log(e);
-}
+const Player = (name, level) => {
+    let health = level * 2;
+    const getLevel = () => level;
+    const getName  = () => name;
+    const die = () => {
+      // uh oh
+    };
+    const damage = x => {
+      health -= x;
+      if (health <= 0) {
+        die();
+      }
+      console.log(health);
+    };
+    const attack = enemy => {
+      if (level < enemy.getLevel()) {
+        damage(1);
+        console.log(`${enemy.getName()} has damaged ${name}`);
+      }
+      if (level >= enemy.getLevel()) {
+        enemy.damage(1);
+        console.log(`${name} has damaged ${enemy.getName()}`);
+      }
+    };
+    return {attack, damage, getLevel, getName};
+  };
+  
+const jimmie = Player('jim', 10);
+const badGuy = Player('jeff', 10);
 
-let child2 = document.querySelector('.child2');
-child2.addEventListener('click', func1);
-
-let links = document.querySelectorAll('li');
-
+jimmie.attack(badGuy);
+jimmie.attack(badGuy);
+jimmie.attack(badGuy);
