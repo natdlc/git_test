60cbca1f1aa551898886f0c6d251535f8aaaf710
nat corpuz <nate.corpuz1@gmail.com>
2022-03-13 19:41:58 +0800

    closure practice

--- script.js
+++ script.js
@@ -7,8 +7,4 @@ let child2 = document.querySelector('.child2');
 child2.addEventListener('click', func1);
 
 let links = document.querySelectorAll('li');
-for (let i = 0; i < links.length; i++) {
-    (function () {
-        console.log(this);
-    }).call(links[i]);
-}
+
