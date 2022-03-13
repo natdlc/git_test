07c932be8286f147b237c17933b195467d5a3ea4
nat corpuz <nate.corpuz1@gmail.com>
2022-03-13 22:14:25 +0800

    practice

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
-const Player = (name, level) => {
-    let health = level * 2;
-    const getLevel = () => level;
-    const getName  = () => name;
-    const die = () => {
-      // uh oh
-    };
-    const damage = x => {
-      health -= x;
-      if (health <= 0) {
-        die();
-      }
-      console.log(health);
-    };
-    const attack = enemy => {
-      if (level < enemy.getLevel()) {
-        damage(1);
-        console.log(`${enemy.getName()} has damaged ${name}`);
-      }
-      if (level >= enemy.getLevel()) {
-        enemy.damage(1);
-        console.log(`${name} has damaged ${enemy.getName()}`);
-      }
-    };
-    return {attack, damage, getLevel, getName};
-  };
+const Person = (name) => {
+    const sayName = () => console.log(`my name is ${name}`);
+    return {sayName};
+  }
   
-const jimmie = Player('jim', 10);
-const badGuy = Player('jeff', 10);
-
-jimmie.attack(badGuy);
-jimmie.attack(badGuy);
-jimmie.attack(badGuy);
+  const Nerd = (name) => {
+    // simply create a person and pull out the sayName function with destructuring assignment syntax!
+    const {sayName} = Person(name);
+    console.log({sayName});
+    const doSomethingNerdy = () => console.log('nerd stuff');
+    return {sayName, doSomethingNerdy};
+}
+  
+  const jeff = Nerd('jeff');
+  
+  jeff.doSomethingNerdy(); // nerd stuff
+  console.log(jeff);
