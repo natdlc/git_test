6f668cd17784755c6c34c32c4cc7004764e551e6
nat corpuz <nate.corpuz1@gmail.com>
2022-03-14 23:34:22 +0800

    edit

--- script.js
+++ script.js
@@ -1,43 +1,8 @@
-let obj1 = {
-  prop1 () {
-    return `function from prop1 in obj1`;
-  },
-  prop2: `string from prop2 in obj1`,
-  prop3: true,
-}
-
-let obj2 = {
-  prop4 () {
-    return `Function from prop4 in obj2`
-  },
-  prop5: `string from prop5 in obj2`,
-  prop6: false,
-}
-
-const returnedObj = Object.assign(Object.create(obj1), obj2)
-
-console.log(`Returned OBJ:`)
-console.log(returnedObj);
-console.log(`\nobj1: `);
-console.log(obj1);
-console.log(`\nobj2: `);
-console.log(obj2);
-
-console.log(returnedObj.prop1());
-
-// const proto = {
-//   hello () {
-//     return `Hello, my name is ${ this.name }`;
-//   }
-// };
-
-// const greeter = (name) => Object.assign(Object.create(proto), {
-//   name
-// });
-
-// const george = greeter('george');
-// console.log(george);
-
-// const msg = george.hello();
-
-// console.log(msg);
+function doSomething() {}
+console.log( doSomething.prototype );
+//  It does not matter how you declare the function; a
+//  function in JavaScript will always have a default
+//  prototype property — with one exception: an arrow
+//  function doesn't have a default prototype property:
+const doSomethingFromArrowFunction = () => {};
+console.log(doSomethingFromArrowFunction.prototype);
