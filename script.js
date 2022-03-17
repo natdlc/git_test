8146ed762a6e29f3d9e1f3e439df7eb088db3d66
nat corpuz <nate.corpuz1@gmail.com>
2022-03-17 23:05:10 +0800

    ending answers unfinished

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function factoryFunc (name) {
-    return {name}
-};
-
-let newObj = factoryFunc('new Obj!');
-
-newObj.greet = function () {
-    console.log(this.name + ' is my name');
+const factoryFunc1 = var1 => {
+    const sayVar = () => console.log(var1);
+    const func1Method = () => console.log('factfunc 1 method');
+    return {sayVar, func1Method};
 }
 
-console.log(newObj);
+const factoryFunc2 = var2 => {
+    const prototype = factoryFunc1(var2);
+    const func2Method = () => console.log('factfunc2 method');
+    return Object.assign({}, prototype, {func2Method});
+}
 
-newObj.greet();
+const newConstant = factoryFunc2('nat');
+newConstant.func2Method();
