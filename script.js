00aad5ac4c5a1040c03c3949f03c8e4a7c4c59d1
nat corpuz <nate.corpuz1@gmail.com>
2022-03-18 13:35:53 +0800

    factory functions and the module pattern

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-const factoryFunc1 = var1 => {
-    const prototypeCreator = () => {
-        const method1 = () => console.log('hello');
-        const method2 = () => console.log('hello');
-        const method3 = () => console.log('hello');
-        const method4 = () => console.log('hello');
-        const method5 = () => console.log('hello');
-        const method6 = () => console.log('hello');
-        return {method1, method2, method3, method4, method5, method6}
+const obj1 = {
+    prop1: 'prop 1',
+    propNest: {
+        propNest1: `prop nest`
     }
-    const prototype = prototypeCreator();
-    const sayVar = () => console.log(var1);
-    const func1Method = () => console.log('factfunc 1 method');
-    return {sayVar, func1Method, prototype};
 }
 
-const factoryFunc2 = var2 => {
-    const prototype = factoryFunc1(var2);
-    const func2Method = () => console.log('factfunc2 method');
-    return Object.assign({}, prototype, {func2Method});
+const obj2 = {
+    prop2: `prop 2`
 }
 
-const newConstant = factoryFunc2('nat');
-console.log(newConstant.prototype.method1());
+const newObj = {...obj1, ...obj2};
+console.log(newObj);
