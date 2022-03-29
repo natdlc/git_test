80ee8225637864e16972e39e1302244b968519ff
nat corpuz <nate.corpuz1@gmail.com>
2022-03-29 13:24:48 +0800

    testing factories

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-const obj = {
-    prop1: 'val 1',
-    prop2: 'val 2'
-}
+const Person = (name, age) => {
+    let _name = name;
+    let _age = age;
+    const getName = () => _name;
+    const getAge = () => _age;
 
-const {prop1, prop2} = obj;
-console.log(prop1);
-console.log(prop2);
+    const addLastName = lastName => {
+        _name = `${_name} ${lastName}`;
+        return _name;
+    };
+
+    const liveForXYears = X => {
+        return _age += +X;
+    }
+    
+    return {
+        getName, 
+        addLastName,
+        getAge,
+        liveForXYears,
+    };
+};
+
+const nodeDeveloper = Person('nat', 26);
+
+console.log(nodeDeveloper.getName());
+console.log(nodeDeveloper.addLastName('corpuz'));
+console.log(nodeDeveloper.getAge());
+console.log(nodeDeveloper.liveForXYears(5));
+console.log(nodeDeveloper.liveForXYears(5));
+console.log(nodeDeveloper.liveForXYears(5));
