1fa983d39615e090a746b41bbe81ae381fb2bf01
nat corpuz <nate.corpuz1@gmail.com>
2022-03-29 16:46:28 +0800

    oop principles incomplete

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-const Person = (name, age) => {
-    let _name = name;
-    let _age = age;
-    const getName = () => _name;
-    const getAge = () => _age;
-
-    const addLastName = lastName => {
-        _name = `${_name} ${lastName}`;
-        return _name;
-    };
-
-    const liveForXYears = X => {
-        return _age += +X;
+const circle = (radius) => {
+    const proto = { 
+      type: 'Circle',
+      //code 
     }
-    
-    return {
-        getName, 
-        addLastName,
-        getAge,
-        liveForXYears,
-    };
-};
+    return Object.assign(Object.create(proto), {radius})
+  }
+  const square = (length) => {
+    const proto = { 
+      type: 'Square',
+      //code 
+    }
+    return Object.assign(Object.create(proto), {length})
+  }
 
-const nodeDeveloper = Person('nat', 26);
-
-console.log(nodeDeveloper.getName());
-console.log(nodeDeveloper.addLastName('corpuz'));
-console.log(nodeDeveloper.getAge());
-console.log(nodeDeveloper.liveForXYears(5));
-console.log(nodeDeveloper.liveForXYears(5));
-console.log(nodeDeveloper.liveForXYears(5));
+  const newCircle = circle(5);
+  console.log(newCircle);
