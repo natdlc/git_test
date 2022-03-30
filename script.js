94f2337719fc59296b871aac2208447c19debf2c
Nat Corpuz <nate.corpuz1@gmail.com>
2022-03-30 11:48:00 +0800

    style incomplete

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const circle = (radius) => {
-    const proto = { 
-      type: 'Circle',
-      //code 
-    }
-    return Object.assign(Object.create(proto), {radius})
-  }
-  const square = (length) => {
-    const proto = { 
-      type: 'Square',
-      //code 
-    }
-    return Object.assign(Object.create(proto), {length})
-  }
-
-  const newCircle = circle(5);
-  console.log(newCircle);
+console.log('hello');
