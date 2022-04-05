e992dd222592451d350f9c5923798f6f047d204f
nat corpuz <nate.corpuz1@gmail.com>
2022-04-06 07:14:27 +0800

    json with localstorage practice

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
-const btn = document.querySelector('button');
-const input = document.getElementById('name');
-const updateStorage = e => {
-  const nameVal = input.value;
-  localStorage.setItem('nameVal', nameVal)
-  console.log(localStorage);
-  insertNameToDom(localStorage.getItem('nameVal'));
-};
+const p1 = document.querySelector('#p1');
+const p2 = document.querySelector('#p2');
 
-const insertNameToDom = nameValue => {
-  const name = document.createElement('h1');
-  name.innerText = nameValue;
-  document.body.appendChild(name);
-}
+const today = 'apr-05-2022';
+const lists = {};
+lists.sublists = {};
 
-btn.addEventListener('click', updateStorage);
+localStorage.setItem(today, JSON.stringify(lists));
 
-/* -=-=-=-=-=-=-=-=-=-=-=- */
-const newFunction = () => {
-  return 'test';
-}
-
-localStorage.setItem('newFunc', newFunction);
-
-const gettingFunc = localStorage.getItem('newFunc');
-console.log(gettingFunc);
-console.log(typeof gettingFunc);
-
-console.log(localStorage);
+p1.addEventListener('keyup', e => {
+  const parsed = JSON.parse(localStorage.getItem(today));
+  parsed.sublists.p1 = e.target.innerText;
+  localStorage.setItem(today, JSON.stringify(parsed));
+  console.log(localStorage.getItem(today));
+})
